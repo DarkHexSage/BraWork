b150e9548f22b6fcb3f57c074aaad3043ae19aaa
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-02-26 11:14:21 -0600

    Update scriptenablementhard.sh
    dd

--- scriptenablementhard.sh
+++ scriptenablementhard.sh
@@ -5,4 +5,4 @@
 DOCKER_HOST_IP=$(hostname -I | cut -d ' ' -f 1)
 
 # Replace occurrences of the IP address with $DOCKER_HOST_IP
-sed -i "s/131.186.0.96/${DOCKER_HOST_IP}/g" /var/www/html/medium-test.php
+sed -i "s/131.186.0.96/${DOCKER_HOST_IP}/g" /var/www/html/hard-test.php
