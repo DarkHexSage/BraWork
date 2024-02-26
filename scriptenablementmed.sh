ce33ffd4c3b4d0c3f199d83cad5d234a61949139
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-02-26 04:53:19 -0600

    Update scriptenablementmed.sh
    lkjkljj\

--- scriptenablementmed.sh
+++ scriptenablementmed.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+
+
+# Get the current value of $DOCKER_HOST_IP
+DOCKER_HOST_IP=$(hostname -I | cut -d ' ' -f 1)
+
+# Replace occurrences of the IP address with $DOCKER_HOST_IP
+sed -i "s/131.186.0.96/${DOCKER_HOST_IP}/g" /var/www/html/easy-test.php
+
+
+
 # Challenge 1: Create an immutable file
 sudo touch /opt/suspiciousfile/keyTrojan
 sudo chattr +i /opt/suspiciousfile/keyTrojan
