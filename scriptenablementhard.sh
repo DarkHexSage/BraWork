71e377e019e5d4cfe1f92c1fef7f0fdade0aeb27
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-02-26 11:10:33 -0600

    Update scriptenablementhard.sh
    dd

--- scriptenablementhard.sh
+++ scriptenablementhard.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+
+# Get the current value of $DOCKER_HOST_IP
+DOCKER_HOST_IP=$(hostname -I | cut -d ' ' -f 1)
+
+# Replace occurrences of the IP address with $DOCKER_HOST_IP
+sed -i "s/131.186.0.96/${DOCKER_HOST_IP}/g" /var/www/html/medium-test.php
