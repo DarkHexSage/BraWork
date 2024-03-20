4860ecdf53e27e3f793d3fa8264699f66c439497
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-03-20 00:50:37 -0600

    Create verify-challengek8s-medium4.php
    G

---
+++ verify-challengek8s-medium4.php
@@ -0,0 +1,27 @@
+<?php
+
+function verifyNetcapodCreation() {
+    // Define the kubectl command to get the pod information
+    $kubectlCommand = 'sudo microk8s.kubectl -n netprod get pod netcapod -o json';
+
+    // Execute the command and capture the output
+    $output = shell_exec($kubectlCommand);
+
+    // Decode the JSON output
+    $podInfo = json_decode($output, true);
+
+    // Check if the pod exists and has the correct properties
+    if ($podInfo && isset($podInfo['metadata']['name']) && $podInfo['metadata']['name'] === 'netcapod' &&
+        isset($podInfo['spec']['containers'][0]['image']) && $podInfo['spec']['containers'][0]['image'] === 'alpine' &&
+        isset($podInfo['spec']['containers'][0]['securityContext']['capabilities']['add']) &&
+        in_array('NET_ADMIN', $podInfo['spec']['containers'][0]['securityContext']['capabilities']['add'])) {
+        return 'success';
+    }
+
+    return 'failure';
+}
+
+// Example usage:
+echo verifyNetcapodCreation();
+?>
+
