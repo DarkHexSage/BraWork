175efd5d54fc092de74e1028d87d032902120b35
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-04-27 18:29:02 -0600

    Update verify-challengek8s-hard1.php

--- verify-challengek8s-hard1.php
+++ verify-challengek8s-hard1.php
@@ -2,7 +2,7 @@
 
 function verifyPVC() {
     // Check if PVC exists
-    $output = shell_exec("sudo microk8s.kubectl get pvc app-data-pvc --no-headers=true");
+    $output = shell_exec("sudo /snap/bin/microk8s.kubectl get pvc app-data-pvc --no-headers=true");
     if (strpos($output, 'app-data-pvc') !== false) {
         return 'success';
     } else {
