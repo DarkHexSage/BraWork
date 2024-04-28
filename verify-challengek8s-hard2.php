541487da7b2107216608194d44db9d690d5405f3
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-04-27 18:28:54 -0600

    Update verify-challengek8s-hard2.php

--- verify-challengek8s-hard2.php
+++ verify-challengek8s-hard2.php
@@ -2,7 +2,7 @@
 
 function verifyServiceAccount() {
     // Check if ServiceAccount exists
-    $output = shell_exec("sudo microk8s.kubectl get sa app-sa --no-headers=true");
+    $output = shell_exec("sudo /snap/bin/microk8s.kubectl get sa app-sa --no-headers=true");
     if (strpos($output, 'app-sa') !== false) {
         return 'success';
     } else {
