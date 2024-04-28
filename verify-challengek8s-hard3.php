a00dcffc0eabc5ac715e1a9c0583f4c34b741dfc
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-04-27 18:28:46 -0600

    Update verify-challengek8s-hard3.php
    /snap/bin/

--- verify-challengek8s-hard3.php
+++ verify-challengek8s-hard3.php
@@ -2,10 +2,10 @@
 
 function verifyResourceLimitation() {
     // Check if Deployment exists
-    $output = shell_exec("sudo microk8s.kubectl get deployment resource-app --no-headers=true");
+    $output = shell_exec("sudo /snap/bin/microk8s.kubectl get deployment resource-app --no-headers=true");
     if (strpos($output, 'resource-app') !== false) {
         // Check if all replicas are running
-        $replicas = shell_exec("sudo microk8s.kubectl get deployment resource-app -o=jsonpath='{.status.readyReplicas}'");
+        $replicas = shell_exec("sudo /snap/bin/microk8s.kubectl get deployment resource-app -o=jsonpath='{.status.readyReplicas}'");
         if ($replicas == "3") {
             return 'success';
         }
