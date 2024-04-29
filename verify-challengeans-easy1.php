2c5dd395a8b46c9de357f7e9302e60944f82c0d6
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-04-28 20:43:45 -0600

    Update verify-challengeans-easy1.php

--- verify-challengeans-easy1.php
+++ verify-challengeans-easy1.php
@@ -1,7 +1,7 @@
 <?php
 
 function verifyHelloTxt() {
-    if (file_exists('/challenge/hello.txt') && trim(file_get_contents('/challenge/hello.txt')) === 'Hello, Ansible!') {
+    if (file_exists('/challenge/hello.txt') && trim(file_get_contents('/challenge/hello.txt')) === 'Hello, Ansible') {
         return 'success';
     } else {
         return 'failure';
