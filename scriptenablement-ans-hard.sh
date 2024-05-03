ef46f4c1208b08108ccd3b894b4866fd842f1e1b
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-05-03 15:36:20 -0600

    Update scriptenablement-ans-hard.sh

--- scriptenablement-ans-hard.sh
+++ scriptenablement-ans-hard.sh
@@ -5,3 +5,4 @@ DOCKER_HOST_IP=$(hostname -I | cut -d ' ' -f 1)
 
 # Replace occurrences of the IP address with $DOCKER_HOST_IP
 sed -i "s/131.186.0.96/${DOCKER_HOST_IP}/g" /var/www/html/hard-ans-test.php
+echo "America/New_York" > /etc/timezone
