1b0eb2acb16f724385138ab5805b3963f3662a61
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-05-04 17:10:04 -0600

    Update easy-k8s-test2.php

--- easy-k8s-test2.php
+++ easy-k8s-test2.php
@@ -122,6 +122,9 @@
         <!-- Exercises -->
         <div id="exercises-container">
             <h1>K8s Exercises Easy 2</h1>
+                <strong>Username:</strong> candidate <br>
+                <strong>Password:</strong> candidate123 <br>
+                <strong><h3>Needle in a haystack</h3></strong>
             <div id="timer">30:00</div> <!-- Timer added here -->
             <div class="challenge">
                            <!-- Challenge 1 -->
