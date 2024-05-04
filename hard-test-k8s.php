c97a9e21a90dbf039767dd1ff0b2bdb1318314d1
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-05-03 18:35:46 -0600

    Update hard-test-k8s.php

--- hard-test-k8s.php
+++ hard-test-k8s.php
@@ -121,7 +121,7 @@
 
         <!-- Exercises -->
         <div id="exercises-container">
-            <h1>K8s Exercises Hard</h1>
+            <h1>K8s Exercises Hard 1</h1>
             <div id="timer">30:00</div> <!-- Timer added here -->
             <div class="challenge">
                            <!-- Challenge 1 -->
