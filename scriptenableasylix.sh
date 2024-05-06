71a5f9aa80e6d1b954212ce879211f4cae97ef5d
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-05-06 10:43:20 -0600

    Update scriptenableasylix.sh

--- scriptenableasylix.sh
+++ scriptenableasylix.sh
@@ -2,7 +2,7 @@
 ##adds user candidate 
 
 # Get the current value of $DOCKER_HOST_IP
-DOCKER_HOST_IP=$(hostname -I | cut -d ' ' -f 1)
+DOCKER_HOST_IP=$(curl ipinfo.io/ip)
 
 # Replace occurrences of the IP address with $DOCKER_HOST_IP
 sed -i "s/131.186.0.96/${DOCKER_HOST_IP}/g" /var/www/html/easy-test.php
