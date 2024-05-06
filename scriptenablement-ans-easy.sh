e93907bc192789d11145adbe1eb55dfd0b574ac8
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-05-06 10:43:28 -0600

    Update scriptenablement-ans-easy.sh

--- scriptenablement-ans-easy.sh
+++ scriptenablement-ans-easy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 chmod 777 /challenge/
 # Get the current value of $DOCKER_HOST_IP
-DOCKER_HOST_IP=$(hostname -I | cut -d ' ' -f 1)
-
+DOCKER_HOST_IP=$(curl ipinfo.io/ip)
 # Replace occurrences of the IP address with $DOCKER_HOST_IP
 sed -i "s/131.186.0.96/${DOCKER_HOST_IP}/g" /var/www/html/easy-ans-test.php
