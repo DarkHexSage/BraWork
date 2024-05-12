1e540ce866eec8838e3e05ac78a4a41a883b0f8f
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-05-11 19:00:57 -0600

    Update scriptenablement-ans-med.sh

--- scriptenablement-ans-med.sh
+++ scriptenablement-ans-med.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-chown -R root:root /challenge/
-chmod 755 /challenge/
+chown -R www-data:www-data /challenge/
+chown -R www-data:www-data /business/
+chmod 700 /challenge/
+chmod 700 /business/
 # Get the current value of $DOCKER_HOST_IP
 DOCKER_HOST_IP=$(curl ipinfo.io/ip)
 # Replace occurrences of the IP address with $DOCKER_HOST_IP
