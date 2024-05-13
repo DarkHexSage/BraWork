415c18020b0910fcb4b31123c9a6c58e77645828
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-05-12 21:21:38 -0600

    Update scriptenablementhard.sh

--- scriptenablementhard.sh
+++ scriptenablementhard.sh
@@ -5,3 +5,8 @@
 DOCKER_HOST_IP=$(curl ipinfo.io/ip)
 # Replace occurrences of the IP address with $DOCKER_HOST_IP
 sed -i "s/131.186.0.96/${DOCKER_HOST_IP}/g" /var/www/html/hard-test.php
+
+chmod 777 /challenge/
+chown -R candidate:candidate /challenge/
+chown -R www-data:www-data /var/www/html/
+chmod 711 /var/www/html/
