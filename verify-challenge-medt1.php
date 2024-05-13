90b5e6534cfa82dda203dbce9f6a280e7faf757c
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-05-12 22:21:47 -0600

    Update verify-challenge-medt1.php

--- verify-challenge-medt1.php
+++ verify-challenge-medt1.php
@@ -1,14 +1,15 @@
- <?php
+<?php
 // Define the file path where flag1 is stored
-$filePath = '/challenge/sortednum.txt';
+$filePath = '/challenge/filediff.txt';
 
 // Compute the MD5 checksum of flag1
 $md5Checksum = trim(shell_exec("md5sum $filePath | cut -d ' ' -f1"));
 
 // Compare the computed MD5 checksum with the expected value
-if ($md5Checksum === '031237c28784bb6594e9e5f9da4efe90') { // Adjust with the correct MD5 checksum
+if ($md5Checksum === 'ae164b5f7ccce3493b5c1914ec05f6e1') { // Adjust with the correct MD5 checksum
     echo 'success';
 } else {
     echo 'failure';
 }
 ?>
+
