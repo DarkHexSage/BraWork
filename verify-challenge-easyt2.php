4ed455d50c273deaf13ea5f7a4e697e0fcae1642
DarkHexSage <156802441+DarkHexSage@users.noreply.github.com>
2024-05-12 20:07:23 -0600

    Update verify-challenge-easyt2.php

--- verify-challenge-easyt2.php
+++ verify-challenge-easyt2.php
@@ -1,12 +1,12 @@
 <?php
 // Define the file path where flag1 is stored
-$filePath = '/challenge/quantity.txt';
+$filePath = '/challenge/concatenated_files.txt';
 
 // Compute the MD5 checksum of flag1
 $md5Checksum = trim(shell_exec("md5sum $filePath | cut -d ' ' -f1"));
 
 // Compare the computed MD5 checksum with the expected value
-if ($md5Checksum === '1f18348f32c9a4694f16426798937ae2') { // Adjust with the correct MD5 checksum
+if ($md5Checksum === '1498ccb6099172d6fd6e9302ffdd63b4') { // Adjust with the correct MD5 checksum
     echo 'success';
 } else {
     echo 'failure';
